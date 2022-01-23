832c669cf4e2ac32bbf861f2362971644d7cd328
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-23 17:41:44 +0300

    гзвфеу

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import "./App.css";
 import RecursiveButton from "./components/RecursiveButton";
 import RecursiveEndlessButton from "./components/RecursiveEndlessButtons";
 class App extends PureComponent {
-  constructor(){
+  constructor() {
     super();
-    this.state={clearAll: false}
+    this.state = { clearAll: false };
     this.handleClick = this.handleClick.bind(this);
   }
   render() {
     return (
       <div className="App">
-        <div className="button-wrapper">
-          <h2>Обратимое порождение, один потомок</h2>
-          <RecursiveButton />
-        </div>
-        <div className="button-wrapper">
-          <h2>Бесконечное порождение и потомки</h2>
-          <button className="buttonRemove" onClick={this.handleClick}>Очистить</button>
-          <RecursiveEndlessButton clearAll={this.state.clearAll}/>
-        </div>
+        <h1>l6_t6, задание 1: рекурсивный вывод кнопок</h1>
+        <p></p>
+        <section>
+          <div className="button-wrapper">
+            <h2>Обратимое порождение, один потомок</h2>
+            <RecursiveButton />
+          </div>
+          <div className="button-wrapper">
+            <h2>Бесконечное порождение и потомки</h2>
+            <button className="buttonRemove" onClick={this.handleClick}>
+              Очистить
+            </button>
+            <RecursiveEndlessButton clearAll={this.state.clearAll} />
+          </div>
+        </section>
       </div>
     );
   }
-  handleClick(){
-      this.setState(prevState=>({clearAll: !prevState.clearAll}))
+  handleClick() {
+    this.setState((prevState) => ({ clearAll: !prevState.clearAll }));
   }
 }
 
